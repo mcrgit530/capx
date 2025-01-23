261544f3343c5451bfa1cef85e6a887587971a12
MULE CHENNAKESAVA REDDY <164854745+mcrgit530@users.noreply.github.com>
2025-01-23 12:23:41 +0530

    Update stockService.js

--- src/services/stockService.js
+++ src/services/stockService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL =process.env.REACT_APP_BACKEND_URL + "/api/stocks";
+const BASE_URL ="http://4.236.218.156:8080/api/stocks";
 const stockService = {
   getAllStocks: async () => {
     const response = await axios.get(`${BASE_URL}`);
